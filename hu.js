d372fe2a1dcfc2baf7d86371573890780b32b813
FreshhteNazzarii <138739762+FreshhteNazzarii@users.noreply.github.com>
2024-01-05 07:51:04 -0600

    Update hu.js

--- hu.js
+++ hu.js
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    int myInt = 19;
+    int myInt = 1239;
     double myDouble = myInt; // Automatic casting: int to double
 
     System.out.println(myInt);      // Outputs 9
