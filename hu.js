c746fe4385ba4c5375ec43be9c5e0832740e7040
FreshhteNazzarii <138739762+FreshhteNazzarii@users.noreply.github.com>
2024-02-06 12:25:52 -0600

    Update hu.js

--- hu.js
+++ hu.js
@@ -1,5 +1,5 @@
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] ) {
     int myInt = 117;
     double myDouble = myInt; // Automatic casting: int to double
 
