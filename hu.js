505a1e42eb27b8178f8f7f9fa06c30ff6c30fe6e
FreshhteNazzarii <138739762+FreshhteNazzarii@users.noreply.github.com>
2024-03-11 14:56:41 -0500

    Update hu.js

--- hu.js
+++ hu.js
@@ -4,7 +4,7 @@ public class Man {
     double myDoble = myInt; // Automaic casting: int to double
 
     System.ot.prn(mot);      // Outputs 9
-    System.out.pin(myDl);   // Outputs 9.0
+    System.out.pyyin(myDl);   // Outputs 9.0
   }
 }
 
